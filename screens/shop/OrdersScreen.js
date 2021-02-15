0ee387aa9b299550cf406b543434ba43690861cd
contrecc <colinccontreary@gmail.com>
2021-02-14 16:20:20 -0800

    Add order fetching from Firebase

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,12 +1,67 @@
-import React from "react";
-import { FlatList, Text, Platform } from "react-native";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState, useCallback } from "react";
+import {
+  FlatList,
+  Text,
+  Platform,
+  StyleSheet,
+  View,
+  ActivityIndicator,
+  Button,
+} from "react-native";
+import { useSelector, useDispatch } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../../components/UI/HeaderButton";
 import OrderItem from "../../components/shop/OrderItem";
+import * as ordersActions from "../../store/actions/orders";
+import Colors from "../../constants/Colors";
 
 const OrdersScreen = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
   const orders = useSelector((state) => state.orders.orders);
+
+  const dispatch = useDispatch();
+
+  const loadOrders = useCallback(async () => {
+    setError(null);
+    setIsLoading(true);
+    try {
+      await dispatch(ordersActions.fetchOrders());
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, [dispatch, setIsLoading, setError]);
+
+  useEffect(() => {
+    loadOrders();
+  }, [dispatch, loadOrders]);
+
+  if (error) {
+    return (
+      <View style={styles.centered}>
+        <Text>An error occurred!</Text>
+        <Button title="Try again" onPress={loadOrders} color={Colors.primary} />
+      </View>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
+
+  if (!isLoading && orders.length === 0) {
+    return (
+      <View style={styles.centered}>
+        <Text>No orders found. Maybe start adding some.</Text>
+      </View>
+    );
+  }
 
   return (
     <FlatList
@@ -39,5 +94,13 @@ OrdersScreen.navigationOptions = (navData) => {
     ),
   };
 };
+
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default OrdersScreen;
--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,4 +1,42 @@
 export const ADD_ORDER = "ADD_ORDER";
+export const SET_ORDERS = "SET_ORDERS";
+
+import Order from "../../models/order";
+
+export const fetchOrders = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        "https://react-native-project-3773e-default-rtdb.firebaseio.com/orders/u1.json"
+      );
+
+      if (!response.ok) {
+        throw new Error(
+          "An error occurred fetching the orders from the database."
+        );
+      }
+
+      const data = await response.json();
+
+      const loadedOrders = [];
+
+      for (const key in data) {
+        loadedOrders.push(
+          new Order(
+            key,
+            data[key].cartItems,
+            data[key].totalAmount,
+            new Date(data[key].date)
+          )
+        );
+      }
+
+      dispatch({ type: SET_ORDERS, orders: loadedOrders });
+    } catch (error) {
+      throw error;
+    }
+  };
+};
 
 export const addOrder = (cartItems, totalAmount) => {
   return async (dispatch) => {
